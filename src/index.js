80f2dd34f3979a6b0e7656558f778efb375adae6
Dragonnx54 <ernesto_ramos_lopez99@hotmail.com>
2025-02-23 22:14:51 -0700

    robofriends redux example course

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import 'tachyons';
 
 
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { requestRobots, searchRobots } from './reducers'
 
 import './index.css';
@@ -33,4 +33,4 @@ root.render(
 //   </Provider>,
 //   document.getElementById('root')
 // );
-registerServiceWorker();
+serviceWorkerRegistration.register();
